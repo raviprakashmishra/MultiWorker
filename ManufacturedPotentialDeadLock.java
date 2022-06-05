054ccc5d02bd4ffb4f19b40ce6c709a8bc1b0b36
Ravi Prakash Mishra <ravi.nitp.ece@gmail.com>
2022-06-05 15:48:19 -0700

    Create ManufacturedPotentialDeadLock.java

---
+++ ManufacturedPotentialDeadLock.java
@@ -0,0 +1,79 @@
+package com.ravi.listeners;
+
+import java.util.ArrayList;
+
+public class MyEvent {
+    MyList<IListener> list;
+    Boolean isFinished = false;
+
+    MyEvent() {
+        list = new MyList<>();
+    }
+
+    public void registerCallback(IListener listener) {
+        produceEvents(listener);
+    }
+
+    public void produceEvents(IListener listener) {
+        synchronized (isFinished) {
+            synchronized (list) {
+                list.addItem(listener, () -> {
+                    if (isFinished) {
+                        eventDone();
+                    }
+                });
+            }
+        }
+
+
+    }
+
+    public void startEvent() {
+        // logic here
+
+        eventDone();
+    }
+
+    public void eventDone() {
+        executeCallBacks();
+    }
+
+    private void executeCallBacks() {
+        synchronized (list) {
+            new Thread(() -> {
+                for (IListener listener: list) {
+                    listener.done();
+                    // this produces ConcurrentModificationException
+                    // concurrent exception can also be there even if we use fail-safe iterator because while call backs are being
+                    // executed at the same time someone may try to add a new listener to the list
+                    list.remove(listener);
+                }
+
+                synchronized (isFinished) {
+                    if (!isFinished)  {
+                        isFinished = true;
+                    }
+                }
+
+
+            }).start();
+        }
+    }
+
+}
+
+class MyList<K> extends ArrayList<K> {
+    public boolean addItem(K o, ItemAdded itemAdded) {
+        boolean added = super.add(o);
+        if (added) {
+            itemAdded.itemAdded();
+        }
+        return added;
+    }
+}
+
+interface ItemAdded {
+    public void itemAdded();
+}
+
+
