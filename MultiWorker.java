9a84699e69b41e2884bbe431a87ba7f15a51050a
Ravi Prakash Mishra <ravi.nitp.ece@gmail.com>
2022-06-04 20:35:20 -0700

    Update MultiWorker.java

--- MultiWorker.java
+++ MultiWorker.java
@@ -1,10 +1,10 @@
 package com.ravi.multiworker;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class MultiWorker {
     BlockingQueue<Worker> workQueue ;
@@ -15,30 +15,39 @@ public class MultiWorker {
         this.list = new ArrayList<>();
     }
 
-    public void start(ICallback callback) {
-        while (true) {
-            if (workQueue.isEmpty()) {
-                // all work done
+    public void start(ICallback callback) throws ExecutionException, InterruptedException {
+        Future<List<CompletableFuture<Boolean>>> future = Executors.newSingleThreadExecutor().submit(new MultiWorkerThread(workQueue));
+        List<CompletableFuture<Boolean>> list = future.get();
+
+        CompletableFuture<Void> allFuture = CompletableFuture.allOf(list.toArray(new CompletableFuture[0]));
+        
+        allFuture.whenComplete((a, e) -> {
+            if (e == null) {
+                callback.onFailure(e);
+            } else {
                 callback.onSuccess(new Result());
-                break;
             }
-        }
+        }) ;
+
 
     }
 
 }
 
-class MultiWorkerThread implements Runnable {
+class MultiWorkerThread implements Callable {
     BlockingQueue<Worker> workQueue;
     MultiWorkerThread (BlockingQueue<Worker> workQueue) {
         this.workQueue = workQueue;
     }
 
     @Override
-    public void run() {
+    public List<CompletableFuture<Boolean>> call() {
+        List<CompletableFuture<Boolean>> list = new ArrayList<>();
         while (!workQueue.isEmpty()) {
             Worker worker = workQueue.poll();
-            worker.start(worker.callback);
+            list.add(worker.start(worker.callback));
         }
+
+        return list;
     }
 }
